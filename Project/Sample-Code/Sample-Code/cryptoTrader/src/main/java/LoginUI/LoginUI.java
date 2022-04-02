673da0933d52f0397180c6dad53c90e7d90966c1
nfilep <100090666+nfilep@users.noreply.github.com>
2022-04-02 17:30:02 -0400

    Add files via upload

--- Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/LoginUI/LoginUI.java
+++ Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/LoginUI/LoginUI.java
@@ -2,6 +2,8 @@ package LoginUI;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
@@ -14,17 +16,32 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
+import MainUI.MainUI;
+import Database.Database;
+
 public class LoginUI extends JFrame implements ActionListener{
+	private final int MAX_ATTEMPTS = 3; // The maximum incorrect login attempts allowed
+	
 	private static LoginUI instance;
-	private JPanel north = new JPanel(); // Set top bar
+	private static JFrame frame = null;
+	private static Database db = Database.getInstance("users.txt");
+	
+	private JPanel mainPanel = new JPanel(new GridLayout(3,1,10,10));
+	
+	private JPanel loginPanel = new JPanel(new FlowLayout());
 	private JButton loginButton = new JButton("Login");
+	
+	private JPanel usernamePanel = new JPanel(new FlowLayout());
 	private JLabel usernameLabel = new JLabel("Username"); 
 	private JTextField usernameInputField = new JTextField("");
+	
+	private JPanel passwordPanel = new JPanel(new FlowLayout());
 	private JLabel passwordLabel = new JLabel("Password"); 
 	private JPasswordField passwordInputField = new JPasswordField(""); 
-	private JPanel south = new JPanel();
-	private JPanel usernamePanel = new JPanel(new BorderLayout());
-	private JPanel passwordPanel = new JPanel(new BorderLayout());
+	
+	private int numAttempts = 0; // The number of times the user has attempted to log in
+	
+	private boolean proceed = false;
 	
 	public static LoginUI getInstance() {
 		if (instance == null)
@@ -35,35 +52,40 @@ public class LoginUI extends JFrame implements ActionListener{
 	private LoginUI() {
 		super("Bitconnect Login"); // Set window title
 		
-		loginButton.setActionCommand("refresh");
-		loginButton.addActionListener(this);
-		
-		north.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Welcome to Bitconnect!",
+		// Create a greeting message
+		mainPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Welcome to Bitconnect!",
 				TitledBorder.CENTER, TitledBorder.TOP));
 		
-		usernamePanel.add(usernameLabel, BorderLayout.LINE_START);
-		usernameInputField.setPreferredSize(new Dimension(150,20));
-		usernamePanel.add(usernameInputField, BorderLayout.LINE_END);
+		// Set up the label and input field for username
+		usernamePanel.add(usernameLabel);
+		usernameInputField.setPreferredSize(new Dimension(100,20));
+		usernamePanel.add(usernameInputField);
 		
-		passwordPanel.add(passwordLabel, BorderLayout.LINE_START);		
+		// Set up the label and input field for password
+		passwordPanel.add(passwordLabel);		
 		passwordInputField.setEchoChar('*');
-		passwordInputField.setPreferredSize(new Dimension(150,20));
-		passwordPanel.add(passwordInputField, BorderLayout.LINE_END);
-		south.add(loginButton);
+		passwordInputField.setPreferredSize(new Dimension(100,20));
+		passwordPanel.add(passwordInputField);
 		
-		north.add(usernamePanel, BorderLayout.NORTH);
-		north.add(passwordPanel, BorderLayout.CENTER);
-		getContentPane().add(north, BorderLayout.NORTH);
-		//getContentPane().add(usernamePanel, BorderLayout.NORTH);
-		//getContentPane().add(passwordPanel, BorderLayout.CENTER);
-		getContentPane().add(south, BorderLayout.SOUTH);
+		// Set up the login button
+		loginButton.setActionCommand("refresh");
+		loginButton.addActionListener(this);
+		loginButton.setPreferredSize(new Dimension(100,20));
+		loginPanel.add(loginButton, BorderLayout.CENTER);
+		
+		// Add the panels to the main panel
+		mainPanel.add(usernamePanel);
+		mainPanel.add(passwordPanel);
+		mainPanel.add(loginPanel);
+		
+		getContentPane().add(mainPanel);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		String command = e.getActionCommand();
 		String password = passwordInputField.getText();
+		
 		if ("refresh".equals(command)) {
 			String username = usernameInputField.getText();
 			if(username.equals("")) {
@@ -74,16 +96,44 @@ public class LoginUI extends JFrame implements ActionListener{
 				return;
 			}else {
 				// Enter code to authenticate the user
-				System.out.println(username);
-				System.out.println(password);
+				if(numAttempts < MAX_ATTEMPTS) {
+					//Boolean valid = authenticate(username, password);
+					if(authenticate(username, password)) {
+						frame.setVisible(false);
+						//JFrame mainframe = MainUI.getInstance();
+						//mainframe.setVisible(true);
+						proceed = true;
+						return;
+					}else {
+						JOptionPane.showMessageDialog(this, "Invalid username or password");
+						numAttempts++;
+						return;
+					}
+				}else {
+					JOptionPane.showMessageDialog(this, "Maximum login attempts exceeded");
+					frame.setVisible(false);
+					return;
+				}
+				
 			}
 		}
 	}
 	
+	private boolean authenticate(String username, String password) {
+		if(db.lookupUsername(username)) {
+			return db.verifyPassword(username, password);
+		}
+		return false;
+	}
+	
+	public boolean getProceed() {
+		return proceed;
+	}
+		
 	public static void main(String[] args) {
-		JFrame frame = LoginUI.getInstance();
-		frame.setSize(900, 600);
-		frame.pack();
+		frame = LoginUI.getInstance();
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setSize(400, 225);
 		frame.setVisible(true);
 	}
 }
