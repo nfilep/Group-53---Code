2f6debe4be2dc275d9ea6093dd156d5633a8ba69
TAgbamuche <84363805+TAgbamuche@users.noreply.github.com>
2022-04-06 23:16:10 -0400

    Add files via upload

--- Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/Trade/TradeResult.java
+++ Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/Trade/TradeResult.java
@@ -5,7 +5,20 @@ import java.util.Arrays;
 
 import Trade.strategies.TradingStrategy;
 
+/** holds informations related to the trade executed
+ * @author Isaac
+ */
 public class TradeResult {
+	/**
+	 * strategy 		The strategy used from A-E
+	 * coinTraded 	The coin traded
+	 * coinPrice 	the price of the coin
+	 * action    	either buying or selling of coins
+	 * quantity  	how many coins traded
+	 * timeStamp 	current date of trade
+	 * success   	whether trade successful or not
+	 */
+	 */
 	private String trader;
 	private TradingStrategy strategy;
 	private String coinTraded;
@@ -15,6 +28,16 @@ public class TradeResult {
 	private String timeStamp;
 	private boolean success;
 	
+	/**
+	 * constructor class
+	 * @param strategy 		The strategy used from A-E
+	 * @param coinTraded 	The coin traded
+	 * @param coinPrice 	the price of the coin
+	 * @param action    	either buying or selling of coins
+	 * @param quantity  	how many coins traded
+	 * @param timeStamp 	current date of trade
+	 * @param success   	whether trade successful or not
+	 */
 	public TradeResult(TradingStrategy strategy, String coinTraded, Double coinPrice, String action, Double quantity, String timeStamp, boolean success) {
 		trader = "N/A";
 		this.strategy = strategy;
@@ -26,34 +49,58 @@ public class TradeResult {
 		this.success = success;
 	}
 	
+	/**
+	 * @return trader name of the broker's result
+	 */
 	public String getTrader() {
 		return trader;
 	}
 	
+	/**
+	 * @param the name of the trader
+	 */
 	public void setTrader(String trader) {
 		this.trader = trader;
 	}
 	
+	/**
+	 * @return the date of the trade
+	 */
 	public String getTimeStamp() {
 		return timeStamp;
 	}
 	
+	/**
+	 * @return strategy used
+	 */
 	public TradingStrategy getStrategy() {
 		return strategy;
 	}
 	
+	/**
+	 * @return coinTraded
+	 */
 	public String getCoinTraded() {
 		return coinTraded;
 	}
 	
+	/**
+	 * @return coinPrice
+	 */
 	public Double getCoinPrice() {
 		return coinPrice;
 	}
 	
+	/**
+	 * @return if trade was successful
+	 */
 	public boolean getSuccess() {
 		return success;
 	}
 	
+	/**
+	 * @return all the information from the trade result in an array row
+	 */
 	public Object[] getData() {
 		Object[] row = {trader, strategy.toString(), coinTraded, action, quantity, coinPrice, timeStamp}; 
 		return row;
--- Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/Trade/TradingBroker.java
+++ Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/Trade/TradingBroker.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 import Trade.strategies.*;
 
+/**
+ * Returns a TradeResult object depending on the information obtained by performing a trade 
+ * @author Isaac
+ */
 public class TradingBroker {
 	/**
-	 * 
+	 * name			the broker's name
+	 * coinList		an arrayList of coins added
+	 * strategy		 the type of strategy invoked
+	 * numSuccessful Trades number of successful trades
 	 */
 	private String name;
 	private ArrayList<String> coinList;
@@ -14,6 +21,12 @@ public class TradingBroker {
 	private TradingStrategy strategy;
 	private int numSuccessfulTrades;
 	
+	/**
+	 * @param name   	 the name of the coin
+	 * @param coinList	 an arraylist of coins
+	 * @param strategy   the type of strategy invoked
+	 * 
+	 */
 	public TradingBroker(String name, ArrayList<String> coinList, TradingStrategy strategy) {
 		this.name = name;
 		this.coinList = coinList;
@@ -22,30 +35,54 @@ public class TradingBroker {
 		numSuccessfulTrades = 0;
 	}
 	
+	/**
+	 * @return name of broker
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * @return coinList
+	 */
 	public ArrayList<String> getCoinList() {
 		return coinList;
 	}
 	
+	/**
+	 * adds the coin to the coinList
+	 * @param coin the name of the coin
+	 */
 	public void addCoin(String coin) {
 		coinList.add(coin);
 	}
 	
+	/**
+	 * @return coinPriceList
+	 */
 	public ArrayList<Double> getCoinPriceList() {
 		return coinPriceList;
 	}
 	
+	/**
+	 * @param price
+	 * adds a specific price to coinPriceList
+	 */
 	public void addPrice(Double price) {
 		coinPriceList.add(price);
 	}
 	
+	/**
+	 * @return strategy  (strategy used)
+	 */
 	public TradingStrategy getStrategy() {
 		return strategy;
 	}
 	
+	/**
+	 * initiates the strategy for a single broker
+	 * @return result (used in displaying)
+	 */
 	public TradeResult performTrade() {
 		TradeResult result = strategy.trade(coinList, coinPriceList);
 		result.setTrader(name);
@@ -55,11 +92,21 @@ public class TradingBroker {
 		return result;
 	}
 	
+	/**
+	 * @return number of successful trade
+	 */
 	public int getNumSuccessfulTrades() {
 		return numSuccessfulTrades;
 	}
 	
 	@Override
+	/**
+	 *
+	 * @param o
+	 *  equal method
+	 * compares the two trading the brokers
+	 * @return true or false
+	 */
 	public boolean equals(Object o) {
 		return this.name.equals(((TradingBroker)o).getName());
 	}
