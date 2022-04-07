9040c39f5c169704c12d11e73f114809588531df
nfilep <100090666+nfilep@users.noreply.github.com>
2022-04-06 22:10:06 -0400

    Delete TradingStrategy.java

--- Project/Sample-Code/Sample-Code/cryptoTrader/src/main/java/Trade/TradingStrategy.java
+++
@@ -1,9 +0,0 @@
-package Trade.strategies;
-
-import java.util.ArrayList;
-
-import Trade.TradeResult;
-
-public abstract class TradingStrategy {
-	public abstract TradeResult trade(ArrayList<String> coinList, ArrayList<Double> coinPriceList);
-}
